960bc563e22f3b8ac3a48db0ccdb7c562a13a8fc
y-7qk <295749232+y-7qk@users.noreply.github.com>
2026-07-13 12:55:55 +0900

    Fix random position calculation in mouseover event

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.querySelectorAll('.butterfly').forEach(function(el){
 	el.addEventListener('mouseover',function(){
-		x =Math.random * 300
-		y = Math.random * 300
+		x =Math.random() * 300
+		y = Math.random() * 300
 		el.style.left = x + "py"
 		el.style.top = y + "py"
 	});
